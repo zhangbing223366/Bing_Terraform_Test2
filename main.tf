1ba5e8bef9a11df8d9bb40cc9ae5c421f9198427
zhangbing223366 <zhangbing595581@hotmail.com>
2025-11-26 16:57:00 +0900

    student
    Added Snowflake provider and resources for database, schema, and table.

--- main.tf
+++ main.tf
@@ -1,17 +1,52 @@
 terraform {
   required_providers {
-    random = {
-      source = "hashicorp/random"
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "~> 0.100"
     }
   }
 }
 
-provider "random" {}
-
-resource "random_pet" "demo" {
-  length = 2
+provider "snowflake" {
+  role      = "ACCOUNTADMIN"
+  warehouse = "COMPUTE_WH"
 }
 
-output "pet_name" {
-  value = random_pet.demo.id
+resource "snowflake_database" "tf_demo_db" {
+  name = "TF_DEMO_DB"
+}
+
+resource "snowflake_schema" "tf_demo_schema" {
+  name     = "TF_DEMO_SCHEMA"
+  database = snowflake_database.tf_demo_db.name
+}
+
+resource "snowflake_table" "students" {
+  database = snowflake_database.tf_demo_db.name
+  schema   = snowflake_schema.tf_demo_schema.name
+  name     = "STUDENTS_TF"
+
+  column {
+    name = "ID"
+    type = "NUMBER"
+  }
+
+  column {
+    name = "NAME"
+    type = "STRING"
+  }
+
+  column {
+    name = "AGE"
+    type = "NUMBER"
+  }
+
+  column {
+    name = "GENDER"
+    type = "STRING"
+  }
+}
+
+output "students_table_fqn" {
+  value = "${snowflake_database.tf_demo_db.name}.${snowflake_schema.tf_demo_schema.name}.${snowflake_table.students.name}"
 }
